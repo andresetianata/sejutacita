f2bcf0852e98cd8c11953dc9098b101864c79862
Andre Setiawan <andresetiawan136@gmail.com>
2021-09-16 20:34:07 +0700

    Progress user (update, delete, read by id) and authentication with token

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 var mongo = require('../db/mongoconnect');
+var token = require('./auth/token')
+const bcrypt = require('bcrypt');
+const saltRound = 10;
+
 var db;
 
 mongo.then(function(connection) {
@@ -7,41 +11,19 @@ mongo.then(function(connection) {
 
 module.exports = {
   api_read_users_all,
-  api_insert_user
+  api_insert_user,
+  api_read_user_by_id,
+  api_update_user,
+  api_delete_user
 }
 
-function api_read_users_all(req, res) {
-  if (db != null) {
-    try {
-      db.collection("users").find({}).toArray(function(err, result) {
-        if (err) throw err;
-        res.json({
-          status: "success",
-          data: result
-        });
-      });
-    }
-    catch(error) {
-      console.log("Error", error)
-      res.json({
-        status: "error",
-        message: "Error read"
-      })
-    }
-  }
-  else {
-    res.json({
-      status: "error",
-      message: "fail to connect database"
-    })
-  }
-}
-function api_insert_user(req, res) {
+async function api_insert_user(req, res) {
   try {
-    console.log("Body",req.body)
     var userObject = {
       "Name": req.body.name,
-      "Role": req.body.role
+      "Role": req.body.role,
+      "Username": req.body.username,
+      "Password": await bcrypt.hash(req.body.password, saltRound)
     }
     db.collection("users").insertOne(userObject, function(err, result) {
       if (err) throw err;
@@ -52,9 +34,159 @@ function api_insert_user(req, res) {
   }
   catch(error) {
     console.log("Error", error)
+    var message = error.message || "Error Insert User";
     res.json({
       status: "error",
-      message: "Error insert"
+      message: message
+    })
+  }
+}
+
+async function api_read_users_all(req, res) {
+  try {
+    if (!req.headers.token) throw { message: "Invalid request"}
+    let checkToken = await token.verifyJWT(req.headers.token);
+    if (checkToken.decoded.Role != "Admin") {
+      throw {
+        message: "Permission denied."
+      }
+    }
+    db.collection("users").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.json({
+        status: "success",
+        data: result
+      });
+    });
+  }
+  catch(error) {
+    console.log("Error", error)
+    var message = (error.message ? error.message : "Error read")
+    res.json({
+      status: "error",
+      message: message
+    })
+  }
+}
+async function api_read_user_by_id(req, res) {
+  try {
+    if (!req.headers.token) throw { message: "Invalid request"}
+    let checkToken = await token.verifyJWT(req.headers.token);
+    if (checkToken.status == "success") {
+      var findParams = {};
+      if (checkToken.decoded.Role == "Admin") {
+        findParams = {
+          "_id": req.params.id
+        };
+      }
+      else {
+        if (req.params.id && req.params.id != checkToken.decoded._id) {
+          throw {
+            message : "Permission denied. Cannot read other user's profile"
+          }
+        }
+        req.params.id = checkToken.decoded._id;
+        findParams = {
+          "_id": req.params.id
+        }
+      }
+
+      db.collection("users").findOne(findParams).toArray(function(error, result) {
+        if (error) throw { message: "Error read from database" }
+        if (result.length == 0) throw { message: "User not found" }
+        res.json({
+          status: "success",
+          data: result[0]
+        })
+      })
+    }
+  }
+  catch(error) {
+    var message = (error.message ? error.message : "Error read")
+    res.json({
+      status: "error",
+      message: message
+    })
+  }
+}
+async function api_update_user(req, res) {
+  try {
+    if (!req.headers.token) throw { message: "Invalid request" }
+    let checkToken = await token.verifyJWT(req.headers.token);
+    if (checkToken.status == "success") {
+      var queryUpdate = {
+        "_id": req.body.id
+      }
+      var newValues = { $set: { } }
+      if (checkToken.decoded.Role == "Admin") {
+        if (req.body.username) newValues["$set"].Username = req.body.username;
+        if (req.body.password) newValues["$set"].Password = req.body.password;
+        if (req.body.name) newValues["$set"].Name = req.body.name;
+      }
+      else {
+        if (req.body.id != checkToken.decoded._id) {
+          throw {
+            message : "Cannot update other user"
+          }
+        }
+        else {
+          if (req.body.username) newValues["$set"].Username = req.body.username;
+          if (req.body.password) newValues["$set"].Password = req.body.password;
+          if (req.body.name) newValues["$set"].Name = req.body.name;
+        }
+      }
+
+      db.collection("users").updateOne(queryUpdate, newValues, function(error, result) {
+        if (error) throw { message: "Error database" }
+        res.json({
+          status: "success"
+        })
+      })
+    }
+  }
+  catch(error) {
+    var message = (error.message ? error.message : "Error update")
+    res.json({
+      status: "error",
+      message: message
+    })
+  }
+}
+async function api_delete_user(req, res) {
+  try {
+    if (!req.headers.token) throw { message: "Invalid request" };
+    let checkToken = await token.verifyJWT(req.headers.token);
+    if (checkToken.status == "success") {
+
+      if (checkToken.decoded.Role == "Admin") {
+        var queryDelete = {
+          "_id": req.body.id
+        }
+        db.collection("users").deleteOne(queryDelete, function(error, result) {
+          if (error) {
+            throw {
+              "message": "Error connect database"
+            }
+          }
+          else {
+            res.json({
+              status: "success"
+            })
+          }
+        });
+      }
+      else {
+        throw {
+          message : "Permission denied. Cannot delete user"
+        }
+      }
+    }
+  }
+  catch(error) {
+    var message = (error.message ? error.message : "Error delete");
+    res.json({
+      status: "error",
+      message: message
     })
   }
 }
--- routers/api.js
+++ routers/api.js
@@ -1,8 +1,13 @@
 const express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var Login = require('../models/Login');
 
 router.get('/user/read_all', User.api_read_users_all);
 router.post('/user/insert', User.api_insert_user)
+router.get('/user/read/:id', User.api_read_user_by_id)
+router.post('/user/update', User.api_update_user)
+router.post('/user/delete', User.api_delete_user)
 
+router.post('/login', Login.api_user_login);
 module.exports = router;
